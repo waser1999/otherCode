36d2a4a5762a63d4a3900f2b0347072a2557a07f
waser1999 <waser1999@outlook.com>
2022-10-21 21:14:41 +0800

    循环

--- Program.cs
+++ Program.cs
@@ -6,13 +6,25 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.Beep();
-            Console.Title = "Formula Studio.";
+            int a;
+            int b;
+            Console.Write("input:");
+            //用户可能输入任何东西，所以得加上错误转换
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("input second number:");
+            b = Convert.ToInt32(Console.ReadLine());
+            int res = a * b;
+            Console.WriteLine("res = " + res);
+
             //命令行打印
             Console.WriteLine("Hello, what's your name?");
             //在变量后打问号，代表此字符串可空
             string? userName = Console.ReadLine();
             Console.WriteLine(userName);
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine(i);
+            }
 
             //关闭程序前悬停
             Console.ReadKey();
