3629f9fe8efe761039c0f17e7916a96c64c2d217
waser1999 <waser1999@outlook.com>
2022-10-21 17:17:15 +0800

    C#编程初步

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Beep();
+            Console.Title = "Formula Studio.";
+            //命令行打印
+            Console.WriteLine("Hello, what's your name?");
+            //在变量后打问号，代表此字符串可空
+            string? userName = Console.ReadLine();
+            Console.WriteLine(userName);
+
+            //关闭程序前悬停
+            Console.ReadKey();
+        }
+    }
+}
