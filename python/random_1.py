034a4aecfd6eecd71cc6d7b0d759355b11d9d6b6
九九 <waser1999@outlook.com>
2023-05-12 22:12:53 +0800

    修复了导入问题

--- python/random_1.py
+++ python/random_1.py
@@ -1,6 +1,6 @@
 import random
 
-with open("list.txt",mode="r",encoding="utf-8") as file:
+with open("python/randomList.txt",mode="r",encoding="utf-8") as file:
     dataList = file.read().split("\n")
     print(random.choice(dataList))
     
