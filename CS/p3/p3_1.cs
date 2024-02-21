e84b6e426d113b7ddec26f42a14a3b9924effd0b
waser1999 <waser1999@outlook.com>
2024-02-21 22:12:12 +0800

    3.1 练习2

--- CS/p3/p3_1.cs
+++ CS/p3/p3_1.cs
@@ -1,4 +1,7 @@
-﻿namespace p3;
+﻿using System.Collections;
+using System.Drawing;
+
+namespace p3;
 
 public class ArrayST<Value>
 {
@@ -41,6 +44,71 @@ public class ArrayST<Value>
         N++;
     }
 }
+public class SequentialSearchST<Value>: IEnumerable<Value?>
+{
+    private class Node{
+        public IComparable index;
+        public Value? value;
+        public Node? next;
+        public Node(IComparable index, Value value, Node next){
+            this.index = index;
+            this.value = value;
+            this.next = next;
+        }
+    }
+    private Node? head;
+    private int N;
+    public int Size(){
+        return N;
+    }
+    public void Put(IComparable index, Value value){
+        if(head == null){
+            head = new Node(index, value, head);
+            N++;
+        }
+
+        for(Node node = head; node != null; node = node.next){
+            if(index.Equals(node.index)) node.value = value;
+        }
+
+        head = new Node(index, value, head);
+        N++;        
+    }
+    public Value? Get(IComparable index){
+        if(head == null) return default(Value);
+        for(Node node = head; node != null; node = node.next){
+            if(index.Equals(node.index)) return node.value;
+        }
+        return default(Value);
+    }
+    public void Delete(IComparable index){
+        if(head == null) return;
+        if(index.Equals(head.index)){
+            head = head.next;
+            N--;
+        }
+
+        for(Node node = head; node != null; node = node.next){
+            if(node.next != null && index.Equals(node.next.index)){
+                node.next = node.next.next;
+                N--;
+            }
+        }
+    }
+
+    public IEnumerator<Value?> GetEnumerator()
+    {
+        if(head == null) yield return default;
+        for(Node node = head; node != null; node = node.next){
+            yield return node.value;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+}
 public class OrderedSequentialSearchST<Value>{
     private class Node{
         public IComparable index;
@@ -53,8 +121,44 @@ public class OrderedSequentialSearchST<Value>{
         }
     }
     private Node? head;
-
+    private int N;
+    public int Size(){
+        return N;
+    }
     public void Put(IComparable index, Value value){
+        if(head == null){
+            head = new Node(index, value, head);
+            N++;
+            return;
+        }
+        
+        if(index.Equals(head.index)){
+            head.value = value;
+            return;
+        }else if(index.CompareTo(head.index) < 0){
+            head = new Node(index, value, head);
+            N++;
+            return;
+        }
+
+        for(Node node = head; node != null; node = node.next){
+            if(node.next !=null){
+                if(index.Equals(node.next.index)){
+                    node.value = value;
+                    N++;
+                    return;
+                }else if(index.CompareTo(node.next.index) < 0){
+                    node.next = new Node(index, value, node.next);
+                    N++;
+                    return;
+                }
+            }else{
+                Node newNode = new Node(index, value, null);
+                node.next = newNode;
+                N++;
+                return;
+            }
+        }
 
     }
     public Value? Get(IComparable index){
@@ -63,13 +167,106 @@ public class OrderedSequentialSearchST<Value>{
         }
         return default(Value);
     }
+    public void Delete(IComparable index){
+        if(head == null) return;
+        
+        if(index.Equals(head.index)){
+            head = head.next;
+            N--;
+            return;
+        }
+
+        for(Node node = head; node != null; node = node.next){
+            if(node.next != null && index.Equals(node.next.index)){
+                node.next = node.next.next;
+                N--;
+                return;
+            }
+        }
+    }
+}
+public class ModifiedBinarySearchST<Value>{
+    private class Item{
+        public IComparable key;
+        public int value;
+    }
+    private Item[] items;
+    public ModifiedBinarySearchST(IComparable[] keys, int[] values){
+        int len = keys.Length;
+        items = new Item[len];
+        for(int i = 0; i < len; i++){
+            items[i] = new Item();
+            items[i].key = keys[i];
+            items[i].value = values[i];
+        }
+        MergeSort(items);
+    }
+    private void MergeSort(Item[] items){
+        Item[] temp = new Item[items.Length];
+
+        MergeSort(items, temp, 0, items.Length - 1);
+    }
+    private void MergeSort(Item[] items, Item[] temp, int low, int high){
+        if(low >= high) return;
+        int middle = low + (high - low) / 2;
+
+        MergeSort(items, temp, low, middle);
+        MergeSort(items, temp, middle + 1, high);
+        Merge(items, temp, low, middle, high);
+    }
+    private void Merge(Item[] items, Item[] temp, int low, int middle, int high){
+        int left = low;
+        int right = middle + 1;
+
+        for(int i = low; i <= high; i++){
+            temp[i] = items[i];
+        }
+
+        for(int i = low; i <= high; i++){
+            if(left > middle){
+                items[i] = temp[right++];
+            }else if(right > high){
+                items[i] = temp[left++];
+            }else if(temp[left].key.CompareTo(temp[right].key) <= 0){
+                items[i] = temp[left++];
+            }else{
+                items[i] = temp[right++];
+            }
+        }
+    }
+    private int BinarySearch(IComparable key, int low, int high){
+        for(int i = low; i <= high; i++){
+            int middle = low + (high - low) / 2;
+            if(key.CompareTo(items[middle].key) > 0){
+                low = middle + 1;
+            }else if(key.CompareTo(items[middle].key) < 0){
+                high = middle - 1;
+            }else{
+                return middle;
+            }
+        }
+        return low;
+    }
+    public void Delete(IComparable key){
+        int index = BinarySearch(key, 0, items.Length);
+        if(index < items.Length && items[index].key.Equals(key)){
+            
+        }
+    }
+    public override string ToString()
+    {
+        string s = "";
+        for(int i = 0; i < items.Length; i++){
+            s += "key:" + items[i].key + ", value:" + items[i].value + "\n";
+        }
+        return s;
+    }
 }
 public class Test{
     static void Main(){
-        ArrayST<int> arrayST = new ArrayST<int>(10);
-        arrayST.Put(12, 34);
-        arrayST.Put(13, 5);
-        arrayST.Delete(13);
-        Console.WriteLine(arrayST.Get(13));
+        IComparable[] keys = {1, 3, 6, 8, 5, 9, 2};
+        int[] values = {34, 22, 4, 5, 32, 55, 6};
+        ModifiedBinarySearchST<int> modifiedBinarySearchST = new ModifiedBinarySearchST<int>(keys, values);
+        Console.WriteLine(modifiedBinarySearchST);
     }
 }
