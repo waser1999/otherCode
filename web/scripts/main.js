d80d11cb6f62249001da08d79413942e467f2c1c
waser1999 <waser1999@outlook.com>
2024-03-01 21:29:20 +0800

    html三件套初学

---
+++ web/scripts/main.js
@@ -0,0 +1,9 @@
+document.querySelector("h1").textContent = "Hello.";
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+document.querySelector("button").addEventListener("click", () =>{
+    alert("It hurts.")
+})
