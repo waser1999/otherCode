d621fb36611ebace1666eb9ba68d72426358b635
waser1999 <waser1999@outlook.com>
2024-03-09 21:25:36 +0800

    html学习

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -1,9 +1,30 @@
 document.querySelector("h1").textContent = "Hello.";
+let myImage = document.querySelector("img");
+let myHeading = document.querySelector("h1");
+let myButton = document.querySelector("button");
 
 function multiply(num1, num2){
     return num1 * num2;
 }
 
-document.querySelector("button").addEventListener("click", () =>{
-    alert("It hurts.")
-})
+myImage.onclick = function(){
+    let oldImage = myImage.getAttribute("src");
+    if(oldImage === "images/firefox-logo.png"){
+        myImage.setAttribute("src", "images/apple.webp");
+    }else{
+        myImage.setAttribute("src", "images/firefox-logo.png");
+    }
+}
+
+function query(){
+    let name = prompt("Please input your name.");
+    if(!name){
+        query();
+    }
+    localStorage.setItem("name", name);
+    myHeading.textContent = "非常感谢！" + name;
+}
+
+myButton.onclick = function(){
+    query();
+}
